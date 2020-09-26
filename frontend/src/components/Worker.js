7478573361466f34384548a16ea1e71101620b09
johnny <johnny231198@gmail.com>
2020-09-26 02:27:05 -0400

    React CRUD and webpack setted

---
+++ frontend/src/components/Worker.js
@@ -0,0 +1,188 @@
+import React from "react";
+import WorkerDataService from "../services/WorkerService";
+
+
+class Worker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: null,
+      name: "",
+      birthdate: "",
+      address: "",
+      phone: "",
+      email: "",
+      dniNumber: "",
+      deparment: ""
+    };
+
+    this.getWorker = this.getWorker.bind(this);
+    this.updateWorker = this.updateWorker.bind(this);
+    this.deleteWorker = this.deleteWorker.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  componentDidMount() {
+    this.getWorker(this.props.match.params.id);
+  }
+
+  componentWillUnmount () {
+    WorkerDataService.cancel();
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  getWorker(id) {
+    WorkerDataService.get(id)
+      .then(response => {
+        this.setState(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  updateWorker() {
+    var data = {
+      name: this.state.name,
+      birthdate: this.state.birthdate,
+      address: this.state.address,
+      phone: this.state.phone,
+      email: this.state.email,
+      dniNumber: this.state.dniNumber,
+      deparment: this.state.deparment
+    };
+
+    WorkerDataService.update(this.state.id, data)
+      .then(response => {
+        console.log(response.data);
+        alert("The worker was updated successfully!");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  deleteWorker() {
+    WorkerDataService.remove(this.state.id)
+      .then(response => {
+        console.log(response.data);
+        alert("The worker was deleted successfully!");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+          <div className="edit-form">
+            <h4>Worker</h4>
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="birthdate">Birthdate</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="birthdate"
+                  name="birthdate"
+                  value={this.state.birthdate}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="address"
+                  name="address"
+                  value={this.state.address}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="phone"
+                  name="phone"
+                  value={this.state.phone}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="dniNumber">DNI number</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="dniNumber"
+                  name="dniNumber"
+                  value={this.state.dniNumber}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="deparment">Deparment</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="deparment"
+                  name="deparment"
+                  value={this.state.deparment}
+                  onChange={this.handleChange}
+                />
+              </div>
+  
+              
+            </form>
+  
+            <button className="badge badge-danger mr-2" onClick={this.deleteWorker}>
+              Delete
+            </button>
+  
+            <button
+              type="submit"
+              className="badge badge-success"
+              onClick={this.updateWorker}
+            >
+              Update
+            </button>
+          </div>
+      </div>
+    );
+  }
+
+}
+
+export default Worker;
